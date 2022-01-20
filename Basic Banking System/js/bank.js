90149402178c5b9f2862298d3feccce47ffca472
Vyshakh_G_NaiR <83595377+VyshakhNaiR@users.noreply.github.com>
2022-01-20 09:29:59 +0530

    Add files via upload

---
+++ Basic Banking System/js/bank.js
@@ -0,0 +1,24 @@
+let myAccountBalance = parseInt(document.getElementById("myAccountBalance").innerText);
+
+function sendMoney(){
+   var enterName = document.getElementById("enterName").value;
+   var enterAmount = parseInt(document.getElementById("enterAmount").value);
+
+   if (enterAmount > 1000000000) {
+      alert("Insufficient Balance.")
+   } else {
+      var findUserBankAccount = "Balance";
+      var finalAmount = parseInt(document.getElementById(findUserBankAccount).innerHTML) + enterAmount;
+      var myAccountBalance = parseInt(document.getElementById("myAccountBalance").innerText) - enterAmount
+      document.getElementById("myAccountBalance").innerText = myAccountBalance
+      document.getElementById(findUserBankAccount).innerHTML = finalAmount;
+      alert(`Transaction was Successful !!   --$${enterAmount} is sent to ${enterName}--`)
+
+ 
+      var createPTag = document.createElement("li");
+      var textNode = document.createTextNode(`$${enterAmount} ------sent------>>>>>  ===={${enterName}}====  ${Date()}.`);
+      createPTag.appendChild(textNode);
+      var element = document.getElementById("transaction-history-body");
+      element.insertBefore(createPTag, element.firstChild);
+   }
+}
